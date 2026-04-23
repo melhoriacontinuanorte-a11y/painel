803b1ca5134c0eacf48acf33c4f3bc4befa63a8b
melhoriacontinuanorte-a11y <melhoriacontinuanorte@gmail.com>
2026-04-23 14:37:31 -0300

    Base local do painel

---
+++ Novo.bat
@@ -0,0 +1,6 @@
+git init
+git branch -M main
+git remote add origin https://github.com/melhoriacontinuanorte-a11y/painel.git
+git add .
+git commit -m "Base local do painel"
+git push -u origin main --force
